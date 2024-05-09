dfbb9cb5df3aebc645557417d4363ad5a34b40f6
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-05-09 08:06:27 +0200

    fix: vercel api custom function

--- api/brew.ts
+++ api/brew.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env DENO_DIR=/tmp deno run
 // Imports
 import { bundle } from "jsr:@libs/bundle@1.0.2/css"
 import { banner as _banner } from "../app/build/css.ts"
@@ -11,9 +12,12 @@ export default async function (request: Request) {
   if (new URL(`https://${request.headers.get("Host")}`).hostname !== new URL(`https://${Deno.env.get("VERCEL_URL") || "localhost"}`).hostname) {
     return new Response(STATUS_TEXT[STATUS_CODE.Forbidden], { status: STATUS_CODE.Forbidden })
   }
-  const body = await request.text()
-  const banner = _banner.replace("matcha.css\n", `matcha.css — Custom build (${new Date().toDateString()})\n`)
-
-  const bundled = await bundle(body, { minify: true, banner, rules: { "no-descending-specificity": false, "no-duplicate-selectors": false } })
-  return new Response(bundled, { headers: { "Content-Type": "text/css" } })
+  try {
+    const body = await request.text()
+    const banner = _banner.replace("matcha.css\n", `matcha.css — Custom build (${new Date().toDateString()})\n`)
+    const bundled = await bundle(body, { minify: true, banner, rules: { "no-descending-specificity": false, "no-duplicate-selectors": false } })
+    return new Response(bundled, { headers: { "Content-Type": "text/css" } })
+  } catch (error) {
+    return new Response(error.message, { status: STATUS_CODE.InternalServerError })
+  }
 }
