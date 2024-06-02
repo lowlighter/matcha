bb81f7a693c87df49d1d4767bcd42552d3bbb77c
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-06-02 02:02:37 +0200

    fix: print error when server crashes

--- app/mod.ts
+++ app/mod.ts
@@ -12,28 +12,33 @@ import api_preview from "../api/preview.ts"
 switch (Deno.args[0]) {
   case "serve":
     Deno.serve(async (request) => {
-      const url = new URL(request.url)
-      switch (true) {
-        case new URLPattern("/{index.html}?", url.origin).test(url.href) && request.headers.get("Accept")?.includes("text/html"):
-          return new Response(await html(), { headers: { "Content-Type": "text/html" } })
-        case new URLPattern("/matcha.css", url.origin).test(url.href):
-          return new Response(await css(), { headers: { "Content-Type": "text/css" } })
-        case new URLPattern("/mod.css", url.origin).test(url.href):
-          return new Response(await Deno.readFile(new URL("mod.css", import.meta.url)), { headers: { "Content-Type": "text/css" } })
-        case new URLPattern("/mod.svg", url.origin).test(url.href):
-          return new Response(await Deno.readFile(new URL("mod.svg", import.meta.url)), { headers: { "Content-Type": "image/svg+xml" } })
-        case new URLPattern("/*.png", url.origin).test(url.href):
-          return new Response(await Deno.readFile(new URL(url.pathname.slice(1), new URL("app/icons/", root))), { headers: { "Content-Type": "image/png" } })
-        case new URLPattern("/*.svg", url.origin).test(url.href):
-          return new Response(await Deno.readFile(new URL(url.pathname.slice(1), new URL("app/icons/", root))), { headers: { "Content-Type": "image/svg+xml" } })
-        case new URLPattern("/styles/*", url.origin).test(url.href):
-          return new Response(await Deno.readFile(new URL(url.pathname.slice(1), root)), { headers: { "Content-Type": "text/css" } })
-        case new URLPattern("/api/brew", url.origin).test(url.href):
-          return api_minify(request)
-        case new URLPattern("/api/preview", url.origin).test(url.href.replace(url.search, "")):
-          return api_preview(request)
-        default:
-          return new Response(STATUS_TEXT[STATUS_CODE.NotFound], { status: STATUS_CODE.NotFound })
+      try {
+        const url = new URL(request.url)
+        switch (true) {
+          case new URLPattern("/{index.html}?", url.origin).test(url.href) && request.headers.get("Accept")?.includes("text/html"):
+            return new Response(await html(), { headers: { "Content-Type": "text/html" } })
+          case new URLPattern("/matcha.css", url.origin).test(url.href):
+            return new Response(await css(), { headers: { "Content-Type": "text/css" } })
+          case new URLPattern("/mod.css", url.origin).test(url.href):
+            return new Response(await Deno.readFile(new URL("mod.css", import.meta.url)), { headers: { "Content-Type": "text/css" } })
+          case new URLPattern("/mod.svg", url.origin).test(url.href):
+            return new Response(await Deno.readFile(new URL("mod.svg", import.meta.url)), { headers: { "Content-Type": "image/svg+xml" } })
+          case new URLPattern("/*.png", url.origin).test(url.href):
+            return new Response(await Deno.readFile(new URL(url.pathname.slice(1), new URL("app/icons/", root))), { headers: { "Content-Type": "image/png" } })
+          case new URLPattern("/*.svg", url.origin).test(url.href):
+            return new Response(await Deno.readFile(new URL(url.pathname.slice(1), new URL("app/icons/", root))), { headers: { "Content-Type": "image/svg+xml" } })
+          case new URLPattern("/styles/*", url.origin).test(url.href):
+            return new Response(await Deno.readFile(new URL(url.pathname.slice(1), root)), { headers: { "Content-Type": "text/css" } })
+          case new URLPattern("/api/brew", url.origin).test(url.href):
+            return api_minify(request)
+          case new URLPattern("/api/preview", url.origin).test(url.href.replace(url.search, "")):
+            return api_preview(request)
+          default:
+            return new Response(STATUS_TEXT[STATUS_CODE.NotFound], { status: STATUS_CODE.NotFound })
+        }
+      }
+      catch (error) {
+        return new Response(`Server was unable to respond to request for the followin reason:\n\n${error}`, { status: STATUS_CODE.InternalServerError })
       }
     })
     break
