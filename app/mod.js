83b16f1f05395f366136d92e756d3f3849e9f0d1
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-07-02 04:47:34 +0200

    fix: attempt to fix color scheme on demo by forcing it on load

--- app/mod.js
+++ app/mod.js
@@ -1,14 +1,26 @@
 /// <reference lib="dom" />
-const prefers = matchMedia("(prefers-color-scheme: dark)") ? "dark" : "light"
+const prefers = matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+const html = document.querySelector("html")
+html.dataset.colorScheme = prefers
+document.querySelectorAll(`body > header .color-scheme`).forEach((element) => {
+  element.querySelector(`svg.${prefers}`).style.display = "inline-block"
+  element.addEventListener("click", () => {
+    toggleColorScheme(html.dataset, element)
+  })
+})
 document.querySelectorAll(".example-tabs li.color-scheme").forEach((element) => {
   element.querySelector(`svg.${prefers}`).style.display = "inline-block"
-  element.addEventListener("click", (_) => {
+  element.addEventListener("click", () => {
     const example = element.parentNode.nextElementSibling
     if (example.tagName === "IFRAME") {
       example.contentWindow.document.documentElement.dataset.colorScheme = (example.contentWindow.document.documentElement.dataset.colorScheme ?? prefers) === "light" ? "dark" : "light"
     }
-    example.dataset.colorScheme = (example.dataset.colorScheme ?? prefers) === "light" ? "dark" : "light"
-    element.querySelector("svg.light").style.display = example.dataset.colorScheme === "light" ? "inline-block" : "none"
-    element.querySelector("svg.dark").style.display = example.dataset.colorScheme === "dark" ? "inline-block" : "none"
+    toggleColorScheme(example.dataset, element)
   })
 })
+
+function toggleColorScheme(dataset, element) {
+  dataset.colorScheme = (dataset.colorScheme ?? prefers) === "light" ? "dark" : "light"
+  element.querySelector("svg.light").style.display = dataset.colorScheme === "light" ? "inline-block" : "none"
+  element.querySelector("svg.dark").style.display = dataset.colorScheme === "dark" ? "inline-block" : "none"
+}
